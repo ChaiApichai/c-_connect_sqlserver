953ca0b62644550c43fcc2435916e1272bf73df1
Apichai <Apichain58@email.nu.ac.th>
2019-07-08 10:47:33 +0700

    first commit

---
+++ Csharp_connect_SqlServer/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Csharp_connect_SqlServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxDept = new System.Windows.Forms.ComboBox();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 251);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.DataGridView1_CellMouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Student ID";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(223, 286);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(392, 20);
+            this.textBoxID.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 323);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(223, 316);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(392, 20);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(151, 352);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Department";
+            // 
+            // comboBoxDept
+            // 
+            this.comboBoxDept.FormattingEnabled = true;
+            this.comboBoxDept.Location = new System.Drawing.Point(223, 344);
+            this.comboBoxDept.Name = "comboBoxDept";
+            this.comboBoxDept.Size = new System.Drawing.Size(392, 21);
+            this.comboBoxDept.TabIndex = 3;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(154, 396);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 4;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.ButtonUpdate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(254, 396);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 4;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.ButtonDelete_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(360, 396);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "SaveToDB";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.ButtonSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.comboBoxDept);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxDept;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ Csharp_connect_SqlServer/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient; //จำเป็น อย่าลืม เพราะเชื่อมฐานข้อมูล SQL
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_connect_SqlServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataSet ds = new DataSet(); //สร้าง dataset
+
+        //string cn = "Data Source=.; Initial Catalog=Employee; User ID=sa; Password=123456"; //ที่อยู่ของฐานข้อมูล
+        string cn = @"Data Source=DESKTOP-OS3KIOS\SQLEXPRESS;Initial Catalog=Employee;User ID=sa;Password=123456"; //ที่อยู่ของฐานข้อมูล
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {// เมื่อเปิดโปรแกรม
+            string sql = "SELECT * FROM tblEmployee";
+            SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+            da.Fill(ds, "Employee");
+            dataGridView1.DataSource = ds.Tables["Employee"]; //นำข้อมูลใน ds มาใส่ gridview
+
+            sql = "SELECT * FROM tblDepartment";
+            da = new SqlDataAdapter(sql, cn);
+            da.Fill(ds, "Department");
+            comboBoxDept.DisplayMember = "deptName"; //เลือกว่าจะเอา field ไหนไปโชว์
+            comboBoxDept.ValueMember = "deptID"; // เอาค่าไปเก็บแอบไว้ เพื่อบันทึกลง database
+            comboBoxDept.DataSource = ds.Tables["Department"];
+
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        { //เมื่อเลือกข้อมูลใน grid จะให้ไปแสดงข้อมูลใน text และ combobox
+
+            /*if(e.RowIndex == -1) // ไม่เลือก หรือเลือกส่วนอื่นที่ไม่ใช่ข้อมูล
+            {
+                return;
+            }
+            else
+            {
+                dataGridView1.Rows[e.RowIndex].Selected = true;
+                //วิธีที่ 1 ดึงข้อมูลจาก DataTable
+                DataRow dr = ds.Tables["Employee"].Rows[e.RowIndex];
+                textBoxID.Text = dr["empID"].ToString();
+                textBoxName.Text = dr["empName"].ToString();
+                comboBoxDept.SelectedValue = dr["empDeptID"].ToString();
+
+                //วิธีที่ 2 ดึงข้อมูลจาก DataGridView
+            }*/
+
+        }
+
+        private void DataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex == -1) // ไม่เลือก หรือเลือกส่วนอื่นที่ไม่ใช่ข้อมูล
+            {
+                return;
+            }
+            else
+            {
+                dataGridView1.Rows[e.RowIndex].Selected = true; //เลือกข้อมูลทั้งแถว
+
+                //วิธีที่ 1 ดึงข้อมูลจาก DataTable
+                DataRow dr = ds.Tables["Employee"].Rows[e.RowIndex];
+                textBoxID.Text = dr["empID"].ToString();
+                textBoxName.Text = dr["empName"].ToString();
+                comboBoxDept.SelectedValue = dr["empDeptID"].ToString();
+
+                //วิธีที่ 2 ดึงข้อมูลจาก DataGridView
+                /*DataGridViewRow dgr = dataGridView1.Rows[e.RowIndex];
+                textBoxID.Text = dgr.Cells[0].Value.ToString();
+                textBoxName.Text = dgr.Cells[1].Value.ToString();
+                comboBoxDept.SelectedValue = dgr.Cells[2].Value.ToString();*/
+            }
+        }
+
+        private void ButtonUpdate_Click(object sender, EventArgs e)
+        { //ไว้ insert/update
+            DataRow[] drs = ds.Tables["Employee"].Select("empID='"+textBoxID.Text+"' ");
+
+            if (drs.Length == 0) //ถ้าไม่มีข้อมูล ให้ insert
+            {
+                DataRow dr = ds.Tables["Employee"].NewRow(); //สร้างแถวใหม่
+
+                dr["empID"] = textBoxID.Text; // ใส่ข้อมูลในแถวที่สร้าง
+                dr["empName"] = textBoxName.Text;
+                dr["empDeptID"] = comboBoxDept.SelectedValue;
+
+                ds.Tables["Employee"].Rows.Add(dr); // เอาแภวที่สร้าง ไปเก็บใน table
+            }
+            else // ถ้ามีข้อมูลให้ update
+            {
+                drs[0]["empName"] = textBoxName.Text;
+                drs[0]["empDeptID"] = comboBoxDept.SelectedValue;
+            }
+
+            dataGridView1.DataSource = ds.Tables["Employee"]; //อัพเดทค่าบน datagridview
+
+        }
+
+        private void ButtonDelete_Click(object sender, EventArgs e)
+        { //ลบข้อมูล
+            DataRow[] drs = ds.Tables["Employee"].Select("empID='"+textBoxID.Text+"' ");
+            if (drs.Length == 0) //หาข้อมูลไม่เจอ
+            {
+                MessageBox.Show("ไม่พบข้อมูล");
+            }
+            else //เจอข้อมูลให้ทำการลบ
+            {
+                drs[0].Delete(); //ลบข้อมูลในแถว *แต่แถวยังอยู่*
+
+                //ปรับปรุงฐานข้อมูล ตรงนี้เลย !!!
+                string sql = "SELECT * FROM tblEmployee";
+                SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+                SqlCommandBuilder cb = new SqlCommandBuilder(da);
+                da.Update(ds, "Employee");
+
+                ds.Tables["Employee"].AcceptChanges(); //จะทำให้แถวที่ว่างหายไป
+            }
+        }
+
+        private void ButtonSave_Click(object sender, EventArgs e)
+        { //บันทึกลง database
+            string sql = "SELECT * FROM tblEmployee";
+            SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+            SqlCommandBuilder cb = new SqlCommandBuilder(da);
+            da.Update(ds, "Employee");
+        }
+    }
+}
